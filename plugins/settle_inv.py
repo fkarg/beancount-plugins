99545b2b19c049267c709eb5db3a34584016770c
fkarg <f.karg10@gmail.com>
2020-11-13 20:27:40 +0100

    [p/settle_inv] Created inverse settle-plugin

--- plugins/settle_inv.py
+++ plugins/settle_inv.py
@@ -7,10 +7,10 @@ Example:
 
     plugin "plugins.settle_inv" "Assets:PayPal"
 
-    2017-04-01 * "" ""
+    2017-04-03 * "" ""
         Assets:SPK              -100.00 EUR
+            paypal: 2017-04-01
         Expenses:Something
-            paypal: 2017-04-03
 
     ; becomes
 
@@ -33,37 +33,55 @@ __plugins__ = ['settlement_date']
 
 def settlement_date(entries, options_map, config):
     # config = PayPal account
+    new_entries = []
     errors = []
 
     for index, entry in enumerate(entries):
         if isinstance(entry, data.Transaction):
+            added = False
             for p_index, posting in enumerate(entry.postings):
                 if posting.meta and 'paypal' in posting.meta:
-                    postings = entry.postings
-                    s_date = posting.meta['paypal']
+
+                    s_date = entry.date
                     link = 'paypal-{}'.format(compare.hash_entry(entry))
                     original_account = posting.account
-                    entry.postings[p_index] = entry.postings[p_index]._replace(account=config)
-                    links = set(entry.links).union([link]) \
-                            if entry.links else set([link])
-                    entries[index] = entry._replace(postings=postings)
-                    entries[index] = entry._replace(links=links)
 
-                    new_posting = postings[p_index]
+                    m_links = set(entry.links).union([link]) \
+                            if entry.links else set([link])
+
+                    m_postings = entry.postings
+                    m_postings[p_index] = m_postings[p_index]._replace(meta=dict())
+                    m_postings[p_index] = m_postings[p_index]._replace(account=config)
+                    m_date = posting.meta['paypal']
+
+                    new_posting = entry.postings[p_index]
                     new_posting = new_posting._replace(meta=dict())
 
-                    postings = [
+                    s_postings = [
                         new_posting,
                         new_posting
                     ]
 
-                    postings[0] = postings[0]._replace(account=original_account)
-                    postings[1] = postings[1]._replace(account=config)
-                    postings[1] = postings[1]._replace(units=postings[0].units._replace(number=postings[0].units.number*-1))
+                    s_postings[0] = s_postings[0]._replace(account=original_account)
+                    s_postings[1] = s_postings[1]._replace(account=config)
+                    s_postings[1] = s_postings[1]._replace(units=entry.postings[p_index].units._replace(number=entry.postings[p_index].units.number*-1))
 
-                    entries.append(data.Transaction(entry.meta, s_date,
+                    modified_transaction = data.Transaction(entry.meta, m_date,
+                        entry.flag, '', entry.narration,
+                        entry.tags, m_links, m_postings)
+
+                    settling_transaction = data.Transaction(entry.meta, s_date,
                         entry.flag, '', 'Settle: {}'.format(entry.narration),
-                        entry.tags, set([link]), postings))
+                        entry.tags, set([link]), s_postings)
 
+                    new_entries.append(modified_transaction)
+                    new_entries.append(settling_transaction)
+
+                    added = True
                     break
-    return entries, errors
+
+            if not added:
+                new_entries.append(entry)
+        else:
+            new_entries.append(entry)
+    return new_entries, errors
